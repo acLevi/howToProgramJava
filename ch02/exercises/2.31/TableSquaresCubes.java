b2ef4a9c3fc2d724e67f26dbad701074847e72c4
Levi Fernandes <levilimabr@gmail.com>
2026-08-31 15:11:08 -0500

    cap 2 exercises

---
+++ ch02/exercises/2.31/TableSquaresCubes.java
@@ -0,0 +1,8 @@
+// Exercise 2.31: Program that calculates the squares and cubes of the numbers 
+// from 0 to 10 and prints the resulting values in a table.
+
+public class TableSquaresCubes {
+  public static void name() {
+    
+  }
+}
